d3b5064377a4e47e159f0b0e99a88454985b1127
Kevin Martinez <kkevinmartinez7@gmail.com>
2020-12-25 16:33:42 -0600

    navigation to other views

--- app/src/main/java/org/kevin/uberclone/MainActivity.java
+++ app/src/main/java/org/kevin/uberclone/MainActivity.java
@@ -2,13 +2,41 @@ package org.kevin.uberclone;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
+
+    Button mButtonIAmClient;
+    Button mButtonIAmDriver;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        mButtonIAmClient = findViewById(R.id.btnIAmClient);
+        mButtonIAmDriver = findViewById(R.id.btnIAmDriver);
+
+        mButtonIAmClient.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                goToSelectAuth();
+            }
+        });
+
+        mButtonIAmDriver.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                goToSelectAuth();
+            }
+        });
+    }
+
+    private void goToSelectAuth() {
+        Intent intent = new Intent(MainActivity.this, SelectOptionAuthActivity.class);
+        startActivity(intent);
     }
 }
