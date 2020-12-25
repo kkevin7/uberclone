89904198ef254251ba8978bb77c339da06bd3c08
Kevin Martinez <kkevinmartinez7@gmail.com>
2020-12-25 16:48:22 -0600

    toolbar componet

--- app/src/main/java/org/kevin/uberclone/SelectOptionAuthActivity.java
+++ app/src/main/java/org/kevin/uberclone/SelectOptionAuthActivity.java
@@ -1,14 +1,21 @@
 package org.kevin.uberclone;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
 
 import android.os.Bundle;
 
 public class SelectOptionAuthActivity extends AppCompatActivity {
 
+    Toolbar mToolbar;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_select_option_auth);
+        mToolbar = findViewById(R.id.toolbar);
+        setSupportActionBar(mToolbar);
+        getSupportActionBar().setTitle("Seleccionar opción");
+        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
     }
 }
